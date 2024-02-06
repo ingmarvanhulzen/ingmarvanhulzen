bfa0be5c4d3938671557a0adf339297eb518b945
Ingmar van Hulzen <info@ivhdevelopment.com>
2024-02-06 22:48:51 +0100

    typo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,12 +4,12 @@ export default function Page() {
   return (
     <main className="min-h-screen flex justify-center items-center">
       <LetterJuggler className="w-full max-w-sm md:max-w-2xl lg:max-w-screen-lg text-3xl lg:text-5xl text-neutral-700 leading-normal">
-        Passinated{" "}
-        <strong className="text-neutral-400">Software Engineer</strong> which
+        Passionated{" "}
+        <strong className="text-neutral-400">Software Engineer</strong> who
         loves to explore the{" "}
         <strong className="text-neutral-400">newest</strong> design trends and{" "}
-        <strong className="text-neutral-400">web</strong> technologies. With lot
-        {"’"}s of <strong className="text-neutral-400">experience</strong> in
+        <strong className="text-neutral-400">web</strong> technologies. With
+        lots of <strong className="text-neutral-400">experience</strong> in
         coding.
       </LetterJuggler>
     </main>
